d5d690d28513cad8fd874d3b7147c3a686c04c93
Os <212662790+ozclef@users.noreply.github.com>
2026-07-02 03:36:49 -0600

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -3,7 +3,34 @@
 
 ///   app.js
 ////  hacías algo como
-
+/*
 if(usuario=="admin" && clave=="1234"){
    mostrarCrud();
+}
+*/
+
+export function login(usuario, clave){
+
+    if(usuario==="admin" && clave==="1234"){
+
+        localStorage.setItem("login","true");
+
+        return true;
+
+    }
+
+    return false;
+
+}
+
+export function logout(){
+
+    localStorage.removeItem("login");
+
+}
+
+export function isLogged(){
+
+    return localStorage.getItem("login")==="true";
+
 }
